e6ac315ba0e998a22ac984187a144479decf5049
kidapu <kidapu@gmail.com>
2019-10-09 10:21:23 +0900

    First commit

---
+++ Assets/Scripts/AlembicTest.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UTJ.Alembic;
+
+public class AlembicTest : MonoBehaviour
+{
+	AlembicStreamPlayer _alembic;
+
+
+	void Start()
+	{
+		_alembic = gameObject.GetComponent<AlembicStreamPlayer>();
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown("s"))
+		{
+			StartCoroutine ( _StartAnimation ());
+		}
+	}
+
+	IEnumerator _StartAnimation()
+	{
+		float time =(float) _alembic.startTime;
+		
+		while (true)
+		{
+			// if ((float)_alembic.endTime <= time)
+			if ((float)2.0f <= time)
+			{
+				yield break; 
+			}
+			_alembic.currentTime = time;
+			time += 0.02f;
+			yield return null; 
+		}
+	}
+}
